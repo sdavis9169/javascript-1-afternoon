5665fc9292235b2c18d21f093d7e4378ba65c45b
Sdavis <sdavis@Stevens-MacBook-Pro.local>
2019-03-02 02:22:07 -0700

    inititail commit

---
+++ README.md
@@ -0,0 +1,32 @@
+<img src="https://s3.amazonaws.com/devmountain/readme-logo.png" width="250" align="right">
+
+# Project Summary
+
+In this project, we'll provide practice JavaScript problems to help you better understand the fundamentals.
+
+## Setup
+
+* `Fork` this repository.
+* `Clone` your fork.
+* Open `./practice.js` with your code editor.
+* Open `./index.html` with your browser.
+
+## Directions
+
+Complete all the activities inside of `./practice.js` to make the spec runner pass it's tests. In order to check the progress of the spec runner, open `./SpecRunner.html` with your browser. Remember to commit and push your code often. Good luck!
+
+## Tips and Tricks
+
+One of the biggest tools at the developer's disposal is the browser's built-in debugger. To use it, open `./index.html` with your browser. Another great tool for small and isolated pieces of code is <a href="http://www.pythontutor.com/visualize.html#">Python Tutor</a>. Just make sure to change the code to `JavaScript ES6`.
+
+## Contributions
+
+If you see a problem or a typo, please fork, make the necessary changes, and create a pull request so we can review your changes and merge them into the master repo and branch.
+
+## Copyright
+
+© DevMountain LLC, 2017. Unauthorized use and/or duplication of this material without express and written permission from DevMountain, LLC is strictly prohibited. Excerpts and links may be used, provided that full and clear credit is given to DevMountain with appropriate and specific direction to the original content.
+
+<p align="center">
+<img src="https://s3.amazonaws.com/devmountain/readme-logo.png" width="250">
+</p>
---
+++ practice.js
@@ -0,0 +1,171 @@
+//////////////////PROBLEM 1////////////////////
+
+//Create a function declaration called greeting that
+//accepts name as its only parameter.
+//greeting should return the string "Hello, "
+//plus the value of the name parameter.
+
+function greeting(name) {
+  return `Hello, ${name}`;
+}
+
+//////////////////PROBLEM 2////////////////////
+
+//Rewrite the function greeting as a function expression.
+//Name it newGreeting.
+
+var newGreeting = function (name){
+  return `Hello, ${name}`;
+}
+
+//////////////////PROBLEM 3////////////////////
+
+//Rewrite the function greeting as an arrow function.
+//Name it finalGreeting.
+
+finalGreeting = name => `Hello, ${name}`;
+
+//////////////////PROBLEM 4////////////////////
+
+//Create an array called groceries with the values
+//"apples", "milk", "eggs", "bread"
+
+const groceries = ['apples', 'milk', 'eggs', 'bread'];
+
+//Write a function called doubleCheck that takes in an array
+//as a parameter.
+
+//If the array does not contain "chocolate", add "chocolate".
+//doubleCheck should return the array.
+
+function doubleCheck (arr){
+  return arr.filter('chocolate')
+}
+
+//////////////////PROBLEM 5////////////////////
+
+//Create an object saved to the variable dog.
+//The dog object should have the following properties:
+//name (a string), color: (a string), age (a number),
+//and goodBoy (a boolean).
+
+var dog = {
+  name: 'Max',
+  color: 'White',
+  age: 12,
+  goodBoy: true
+}
+
+//...access the dog's name from the object and assign it to a
+//variable called devMountainClassPet.
+
+var devMountainClassPet = dog.name;
+
+//Add a method to dog called bark.
+//The value of bark should be a function that returns the string "Woof woof".
+
+dog.bark = function () {
+  return "Woof woof"
+}
+
+//Store the result of invoking the bark method in a variable called ruff.
+
+var ruff = dog.bark();
+
+//////////////////PROBLEM 6////////////////////
+
+//Write a function called looper that takes in an array. looper should declare
+//a variable called mySum and set it equal to 0. looper should then loop through
+//the array and check each element.
+
+//If the element is odd, or if the element is greater than or equal to 100, add the element
+//to the mySum variable total.
+
+//Return mySum.
+
+//Code Here
+
+//////////////////PROBLEM 7////////////////////
+
+//Given the following function called math
+
+function math(num1, num2, callback) {
+  return callback(num1, num2);
+}
+
+//Write a function called add that takes in two parameters and
+//returns the result of adding them together.
+
+function add (par1, par2) {
+  return par1 + par2;
+}
+
+//Now invoke math, passing in the numbers 3 and 4, and your add function,
+//storing the result in the variable mathSum.
+
+var mathSum = add(3, 4);
+
+//////////////////PROBLEM 8////////////////////
+
+//Write a function called invoker that takes in one paramter, a callback function.
+//invoker should return the result of invoking the callback.
+
+function sampleCallbackOne() {
+  return "I am a callback function";
+}
+
+function sampleCallbackTwo() {
+  return "I am also a callback function";
+}
+
+//Code Here
+
+//////////////////PROBLEM 9////////////////////
+
+let duck = "cute";
+
+function bathroom() {
+  let rubberDuck = "squeaky";
+  function bathtub() {
+    let sailorDuck = "nautical";
+  }
+}
+
+function pond() {
+  let realDuck = "fluffy";
+}
+
+//There are 4 variables above: duck, rubberDuck, sailorDuck and realDuck
+//all within different scopes.
+//Given the functions and variables above, edit the arrays
+//below to contain only the appropriate variable names
+//as strings.
+
+//This array should contain the variable names (as strings) accessible in the global scope.
+let globalScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
+
+//This array should contain the variable names (as strings) accessible in the bathroom function.
+let bathroomScope = ["duck", "sailorDuck", "rubberDuck"];
+
+//This array should contain the variable names (as strings) accessible in the bathtub function.
+let bathtubScope = ["sailorDuck"];
+
+//This array should contain the variable names (as strings) accessible in the pond function.
+let pondScope = ["realDuck", "duck"];
+
+//////////////////PROBLEM 10////////////////////
+
+//Create a function called outerFn which returns an anonymous
+//function which returns your name.
+
+//Code Here
+function outerFn(){
+  
+}
+//Now save the result of invoking outerFn into a variable called innerFn.
+
+//Code Here
+
+//Now invoke innerFn and save the result to a variable called finalResult.
+
+//Code Here
